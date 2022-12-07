b68fe71159642b4e084bcbe42a10dbafaaf76a32
gabriel <gabrielcmoris@gmail.com>
2022-12-07 10:25:43 +0100

    iban stractor to test iban module

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { jsonMaker } = require("./modules/json-maker");
 const axios = require("axios");
 var cheerio = require("cheerio");
 
-var baseUrl = process.env.WEB_TO_CHECK;
+var baseUrl = "https://formvalidation.io/guide/validators/iban/";
 
 (async () => {
   try {
@@ -17,29 +17,30 @@ var baseUrl = process.env.WEB_TO_CHECK;
 async function getLinksFromURL(url) {
   try {
     let links = [];
+    let countries = [];
+    let ibans = [];
     let httpResponse = await axios.get(url);
 
     let $ = cheerio.load(httpResponse.data);
-    let linkObjects = $("a"); // get all hyperlinks
-    let hrefLink;
-    linkObjects.each((index, element) => {
-      if (
-        $(element).text().trim().replace(" ", "") !== "" &&
-        $(element).attr("href") &&
-        $(element).attr("href") !== "#"
-      ) {
-        // get the href attribute
-        if ($(element).attr("href").substring(0, 4) != "http") {
-          hrefLink = process.env.HOMEPAGE + $(element).attr("href");
-        } else {
-          hrefLink = $(element).attr("href");
-        }
-        links.push({
-          text: $(element).text().trim().replace(" ", ""), // get the text
-          href: hrefLink,
-        });
+    let countriesArr = $("td:nth-child(1)"); // get all countries
+    let ibansArr = $("td:nth-child(4)"); //get all ibans
+    countriesArr.each((index, element) => {
+      if (index > 2) {
+        countries.push($(element).text().trim().replace(" ", ""));
       }
     });
+    ibansArr.each((index, element) => {
+      if (index > 2) {
+        ibans.push($(element).text().trim().replace(" ", ""));
+      }
+    });
+    for (let i = 0; i < countries.length; i++) {
+      links.push({
+        country: countries[i],
+        iban: ibans[i],
+      });
+    }
+
     jsonMaker(links);
     return ">>> results.json was created in folder /results.";
   } catch (e) {
